8ea9932330d45b4dea98e4fadcc8db3bc7706c55
SofiaKitaeva <75208744+SofiaKitaeva@users.noreply.github.com>
2022-01-14 00:29:33 -0300

    Add files via upload

---
+++ MinhaData.java
@@ -0,0 +1,11 @@
+package trab3;
+
+public class MinhaData {
+    int dia;
+    int mes;
+    int ano;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
